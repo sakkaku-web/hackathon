20ced7c90bdf48e54e5f4d88cf1edd6dffbc729a
Min Yin <min.yin@evolit.com>
2022-07-30 19:52:36 +0200

    Add modal and button component

---
+++ apps/hackathon/src/app/components/Modal.tsx
@@ -0,0 +1,24 @@
+import { PropsWithChildren } from 'react';
+import { ReactComponent as CloseIcon } from '../../assets/x.svg';
+import { Button } from './Button';
+
+/* eslint-disable-next-line */
+export interface ModalProps extends PropsWithChildren {
+  buttonText: string;
+  onClose: () => void;
+  onSubmit: () => void;
+}
+
+export function Modal({ buttonText, children, onSubmit, onClose }: ModalProps) {
+  return (
+    <div className="absolute inset-0 bg-white" style={{ zIndex: 999999 }}>
+      <div className="flex flex-row items-center justify-between border-b p-2">
+        <CloseIcon className="h-8 w-8" onClick={() => onClose()} />
+        <Button onClick={() => onSubmit()}>{buttonText}</Button>
+      </div>
+      <div className="p-2">{children}</div>
+    </div>
+  );
+}
+
+export default Modal;
