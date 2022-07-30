927529549de5cb792850f88eef11dfbfe34c54f8
Hao <74541600+haod98@users.noreply.github.com>
2022-07-30 19:52:45 +0200

    Style shautouts

--- apps/hackathon/src/app/components/ShautItem.tsx
+++ apps/hackathon/src/app/components/ShautItem.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { environment } from '../../environments/environment';
 import { ShautMessage } from '@sakkaku-web/core';
+import { format, formatDuration } from 'date-fns';
+import { ReactComponent as DefaultProfilePicture } from '../../assets/person-circle.svg';
 
 interface ShautItemProps {
   item: ShautMessage;
@@ -8,10 +10,21 @@ interface ShautItemProps {
 
 function ShautItem({ item }: ShautItemProps) {
   return (
-    <div>
-      <p>{item.user}</p>
-      <p>{item.text}</p>
-      <p>{item.time}</p>
+    <div className="border-t-2 border-gray-500 p-2">
+      <div className="grid grid-cols-6">
+        <div className="grid justify-center">
+          <DefaultProfilePicture className="h-10 w-10" />
+        </div>
+        <div className="col-start-2 col-end-7">
+          <p className="text-lg font-bold leading-none">{item.user}</p>
+          <p className="leading-[1.2]">{item.text}</p>
+          <div className="flex justify-end">
+            <p className="text-right text-xs text-gray-500">
+              {format(new Date(item.time), 'dd.M.yyyy HH:mm')}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
