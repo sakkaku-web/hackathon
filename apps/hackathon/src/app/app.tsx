e83760519798b9e1a1a4494f84b48b2c4e8365b6
Hao <74541600+haod98@users.noreply.github.com>
2022-07-30 15:09:54 +0200

    Show current location on map

--- apps/hackathon/src/app/app.tsx
+++ apps/hackathon/src/app/app.tsx
@@ -1,11 +1,19 @@
 import OpenMap from './components/OpenMap';
 import styles from './app.module.scss';
+import { MapContainer } from 'react-leaflet';
 
 export function App() {
   return (
     <>
       <h1 className="font-bold text-2xl">Hello World</h1>
-      <OpenMap />
+      <MapContainer
+        center={[48.208492, 16.373755]}
+        zoom={13}
+        scrollWheelZoom={false}
+        className="h-44"
+      >
+        <OpenMap />
+      </MapContainer>
     </>
   );
 }
--- apps/hackathon/src/app/components/OpenMap.tsx
+++ apps/hackathon/src/app/components/OpenMap.tsx
@@ -1,23 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { LatLngExpression } from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 function OpenMap() {
-  const [latitude, setLatitude] = useState(48.208492);
-  const [longitude, setlongitude] = useState(16.373755);
+  const [latLong, setLatLong] = useState<LatLngExpression>([
+    48.208492, 16.373755,
+  ]);
+  function success(pos: GeolocationPosition): void {
+    const coordinates = pos.coords;
+    setLatLong([coordinates.latitude, coordinates.longitude]);
+  }
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
+
+  const map = useMap();
+  useEffect(() => {
+    map.setView(latLong);
+  }, [latLong]);
+
   return (
-    <MapContainer
-      center={[latitude, longitude]}
-      zoom={13}
-      scrollWheelZoom={false}
-      className="h-44"
-    >
+    <>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]}>
+      <Marker position={latLong}>
         <Popup>Your current location</Popup>
       </Marker>
-    </MapContainer>
+    </>
   );
 }
 
